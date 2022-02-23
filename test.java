b59aeb67e948a640f466411ed69c91befbfd37a8
venkatvaranasi8421 <100311800+venkatvaranasi8421@users.noreply.github.com>
2022-02-23 12:30:34 -0800

    Delete test.java

--- test.java
+++
@@ -1,7 +0,0 @@
-class test{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-        System.out.println("Hello Java123");  
-         System.out.println("Hello Java45dfdgfdfg");  
-    }  
-}  
