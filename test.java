70c2d66094acd83d9b5f01f72d270ab08215aaad
venkatvaranasi8421 <100311800+venkatvaranasi8421@users.noreply.github.com>
2022-02-23 11:32:28 -0800

    Fix: fixed message

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ class test{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
         System.out.println("Hello Java123");  
-         System.out.println("Hello Java45");  
+         System.out.println("Hello Java45dfdgfdfg");  
     }  
 }  
